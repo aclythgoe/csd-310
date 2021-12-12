d5471c1ad038614be2c19915be364658a02ce465
aclythgoe <aclythgoe@my365.bellevue.edu>
2021-12-12 11:35:25 -0700

    Andrew Lythgoe December 12, 2021 Module 8 Assignment 8.2 Commit

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -1,28 +1,23 @@
 import mysql.connector
-from mysql.connector import errorcode
-
-
-config = {
-    "user" : "pysports_user",
-    "password" : "MySQL8IsGreat!",
-    "host" : "127.0.0.1",
-    "database" : "pysports",
-    "raise_on_warings" : True
-}
+from mysql.connector import Error
 
 try:
-    db = mysql.connector.connect(config)
+    connection = mysql.connector.connect(host='localhost',
+                                         database='pysports',
+                                         user='pysports_user',
+                                         password='MySQL8IsGreat!')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
 
-    print(f"\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]
-    print("\n\n Press any key to continue...")
-
-
-except mysql.connector.error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print(" The supplies username or password are invalid")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print(" The specified database does not exist")
-    else:
-        print(err)
-    finally:
-        db.close()
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
---
+++ module_8/pysports_queries.py
@@ -0,0 +1,10 @@
+"""
+Andrew Lythgoe
+December 11,2021
+Module 8.3 Assignment
+"""
+
+"""
+Allow connection to Pysports Database
+"""
+
